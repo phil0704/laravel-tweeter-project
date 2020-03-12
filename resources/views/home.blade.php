ce3c07f1afe592982ce98e7d6e924cf6fb62a65d
Philip Querimit <philip@Philips-MacBook-Air.local>
2020-03-12 16:35:39 -0600

    homeblade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,21 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="row">
+      <div class="col-3">
+        <img src="https://cdn.clipart.email/74c59ed4f10f0db36c34dfc09e54f343_twitter-logo-png-small-5-png-image_257-264.png" class="rounded-circle">
+      </div>
+      <div class="col-9 pt-4">
+      <div><h1>Twitter</h1></div>
+      <div class="d-flex">
+          <div class="pr-5"><strong>158</strong> posts</div>
+          <div class="pr-5"><strong>28k</strong> followers</div>
+          <div class="pr-5"><strong>528</strong> following</div>
+      </div>
+      <div class="">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+      </div>
+     </div>
     </div>
 </div>
 @endsection
